f235f95ddf1d30852539abb65ab9606d0e79279b
alexisqm <alexisqm047@gmail.com>
2021-10-05 18:49:11 -0500

    Detalles secretario

--- GOB.App/GOB.App.Frontend/Pages/Listas/DetallesSecretario.cshtml.cs
+++ GOB.App/GOB.App.Frontend/Pages/Listas/DetallesSecretario.cshtml.cs
@@ -9,24 +9,22 @@ using GOB.App.Dominio;
 
 namespace GOB.AppFrontend.Pages
 {
-    public class DetallesModel : PageModel
+    public class DetallesSecretarioModel : PageModel
     {
-        
         private static IRepositorioSecretarioDespacho _repoSecretarioDespacho = new RepositorioSecretarioDespacho(new GOB.App.Persistencia.AppContext());
         public SecretariosDespacho secretario;
 
         public IActionResult OnGet(int secretarioId)
         {
-            
+
             secretario = _repoSecretarioDespacho.GetSecretario(secretarioId);
-            if(secretario==null)
+            if (secretario == null)
             {
                 return RedirectToPage("./Secretarios");
-            }else 
-            return Page();
-        
-        }
+            }
+            else
+                return Page();
 
-        
+        }
     }
 }
---
+++ GOB.App/GOB.App.Frontend/Pages/Listas/Proveedores.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GOB.App.Persistencia;
+using GOB.App.Dominio;
+
+namespace GOB.AppFrontend.Pages
+{
+    public class ProveedoresModel : PageModel
+    {
+        private static IRepositorioProveedor _repoProveedor = new RepositorioProveedor(new GOB.App.Persistencia.AppContext());
+        public IEnumerable<Proveedor> Proveedores{get;set;}
+        public void OnGet()
+        {
+            Proveedores = _repoProveedor.GetAllProveedores();
+        }
+    }
+}
