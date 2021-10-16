45e80ac2dabed107c7a5631b159f869fb55c7b00
julian1065 <juliandresmarinramirez31@gmail.com>
2021-10-16 13:37:10 -0500

    commit Julian

---
+++ GOB.App/GOB.App.Frontend/Pages/Listas/DetallesPersonal.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GOB.App.Persistencia;
+using GOB.App.Dominio;
+
+namespace GOB.AppFrontend.Pages
+{
+    public class DetallesPersonalModel : PageModel
+    {
+        private static IRepositorioPersonalAseo _repoPersonalAseo = new RepositorioPersonalAseo(new GOB.App.Persistencia.AppContext());
+        public PersonalAseo personal;
+
+        public IActionResult OnGet(int personalId)
+        {
+            personal = _repoPersonalAseo.GetEmpleadoAseo(personalId);
+            if (personal == null)
+            {
+                return RedirectToPage("./Personal");
+            }
+            else
+                return Page();
+        }
+    }
+}
